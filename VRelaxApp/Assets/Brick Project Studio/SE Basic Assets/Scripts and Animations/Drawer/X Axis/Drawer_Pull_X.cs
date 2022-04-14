3e1c996b233ba55d61c7a8e03a31347b5e6a6c00
sayhanne <sayhannee@gmail.com>
2022-04-15 01:08:52 +0300

    coffeShop and wristMenu

--- VRelaxApp/Assets/Brick Project Studio/SE Basic Assets/Scripts and Animations/Drawer/X Axis/Drawer_Pull_X.cs
+++ VRelaxApp/Assets/Brick Project Studio/SE Basic Assets/Scripts and Animations/Drawer/X Axis/Drawer_Pull_X.cs
@@ -2,73 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SojaExiles
+public class Drawer_Pull_X: MonoBehaviour {
 
-{
+	public Animator pull_01;
+	public bool open;
+	public Transform Player;
 
-	public class Drawer_Pull_X : MonoBehaviour
-	{
+	void Start (){
+		open = false;
+	}
 
-		public Animator pull_01;
-		public bool open;
-		public Transform Player;
-
-		void Start()
+	void OnMouseOver (){
 		{
-			open = false;
-		}
-
-		void OnMouseOver()
-		{
-			{
-				if (Player)
-				{
-					float dist = Vector3.Distance(Player.position, transform.position);
-					if (dist < 10)
-					{
-						print("object name");
-						if (open == false)
-						{
-							if (Input.GetMouseButtonDown(0))
-							{
-								StartCoroutine(opening());
-							}
+			if (Player) {
+				float dist = Vector3.Distance (Player.position, transform.position);
+				if (dist < 10) {
+                    print("object name");
+					if (open == false) {
+						if (Input.GetMouseButtonDown (0)) {
+							StartCoroutine (opening ());
 						}
-						else
-						{
-							if (open == true)
-							{
-								if (Input.GetMouseButtonDown(0))
-								{
-									StartCoroutine(closing());
-								}
+					} else {
+						if (open == true) {
+							if (Input.GetMouseButtonDown (0)) {
+								StartCoroutine (closing ());
 							}
-
 						}
 
 					}
-				}
 
+				}
 			}
 
 		}
 
-		IEnumerator opening()
-		{
-			print("you are opening the door");
-			pull_01.Play("openpull_01");
-			open = true;
-			yield return new WaitForSeconds(.5f);
-		}
-
-		IEnumerator closing()
-		{
-			print("you are closing the door");
-			pull_01.Play("closepush_01");
-			open = false;
-			yield return new WaitForSeconds(.5f);
-		}
-
-
 	}
+
+	IEnumerator opening(){
+		print ("you are opening the door");
+		pull_01.Play ("openpull_01");
+		open = true;
+		yield return new WaitForSeconds (.5f);
+	}
+
+	IEnumerator closing(){
+		print ("you are closing the door");
+		pull_01.Play ("closepush_01");
+		open = false;
+		yield return new WaitForSeconds (.5f);
+	}
+
+
 }
+
--- VRelaxApp/Assets/Brick Project Studio/SE Basic Assets/Scripts and Animations/Windows/opencloseWindow1.cs
+++ VRelaxApp/Assets/Brick Project Studio/SE Basic Assets/Scripts and Animations/Windows/opencloseWindow1.cs
@@ -2,71 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SojaExiles
+public class opencloseWindow1: MonoBehaviour {
 
-{
-	public class opencloseWindow1 : MonoBehaviour
-	{
+	public Animator openandclosewindow1;
+	public bool open;
+	public Transform Player;
 
-		public Animator openandclosewindow1;
-		public bool open;
-		public Transform Player;
+	void Start (){
+		open = false;
+	}
 
-		void Start()
+	void OnMouseOver (){
 		{
-			open = false;
-		}
-
-		void OnMouseOver()
-		{
-			{
-				if (Player)
-				{
-					float dist = Vector3.Distance(Player.position, transform.position);
-					if (dist < 15)
-					{
-						if (open == false)
-						{
-							if (Input.GetMouseButtonDown(0))
-							{
-								StartCoroutine(opening());
-							}
+			if (Player) {
+				float dist = Vector3.Distance (Player.position, transform.position);
+				if (dist < 15) {
+					if (open == false) {
+						if (Input.GetMouseButtonDown (0)) {
+							StartCoroutine (opening ());
 						}
-						else
-						{
-							if (open == true)
-							{
-								if (Input.GetMouseButtonDown(0))
-								{
-									StartCoroutine(closing());
-								}
+					} else {
+						if (open == true) {
+							if (Input.GetMouseButtonDown (0)) {
+								StartCoroutine (closing ());
 							}
-
 						}
 
 					}
-				}
 
+				}
 			}
 
 		}
 
-		IEnumerator opening()
-		{
-			print("you are opening the Window");
-			openandclosewindow1.Play("Openingwindow 1");
-			open = true;
-			yield return new WaitForSeconds(.5f);
-		}
-
-		IEnumerator closing()
-		{
-			print("you are closing the Window");
-			openandclosewindow1.Play("Closingwindow 1");
-			open = false;
-			yield return new WaitForSeconds(.5f);
-		}
-
-
 	}
+
+	IEnumerator opening(){
+		print ("you are opening the Window");
+		openandclosewindow1.Play ("Openingwindow 1");
+		open = true;
+		yield return new WaitForSeconds (.5f);
+	}
+
+	IEnumerator closing(){
+		print ("you are closing the Window");
+		openandclosewindow1.Play ("Closingwindow 1");
+		open = false;
+		yield return new WaitForSeconds (.5f);
+	}
+
+
 }
+
