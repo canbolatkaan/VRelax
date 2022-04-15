b6373cbcca5d6ed81ce41e2643c1278da4f33ab2
sayhanne <sayhannee@gmail.com>
2022-04-15 18:03:03 +0300

    Login scene and keyboard script updated

--- VRelaxApp/Assets/KeyDetector.cs
+++ VRelaxApp/Assets/KeyDetector.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 
 public class KeyDetector : MonoBehaviour
 {
-    private TextMeshPro playerTextOutput;
-
+    private TextMeshPro emailTextOutput;
+    bool caps_on = false;
     // Start is called before the first frame update
     void Start()
     {
-        playerTextOutput = GameObject.FindGameObjectWithTag("PlayerTextOutput").GetComponentInChildren<TextMeshPro>();
+        emailTextOutput = GameObject.FindGameObjectWithTag("Email").GetComponentInChildren<TextMeshPro>(); // must be fixed. it gives error know.
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,15 +22,24 @@ public class KeyDetector : MonoBehaviour
             var keyFeedback = other.gameObject.GetComponent<KeyFeedback>();
             if (keyFeedback.keyCanBeHitAgain)
             {
-                if (key.text == "SPACE")
-                    playerTextOutput.text += " ";
+                if (key.text == "SPACE") // must be fixed when we are typing in email/password field.cannot contain space!!!
+                    emailTextOutput.text += " ";
                 else if (key.text == "DEL")
                 {
-                    int len = playerTextOutput.text.Length;
-                    playerTextOutput.text = playerTextOutput.text.Substring(0, len - 1);
+                    int len = emailTextOutput.text.Length;
+                    emailTextOutput.text = emailTextOutput.text.Substring(0, len - 1);
                 }
+                else if (key.text == "caps_on LK")
+                    caps_on = !caps_on;
+                
                 else
-                    playerTextOutput.text += key.text;
+                {
+                    if(caps_on)
+                        emailTextOutput.text += key.text.ToUpper();
+                    else
+                        emailTextOutput.text += key.text.ToLower();
+                }
+                    
 
                 keyFeedback.keyHit = true;
             }
