0f27252ef1c72e6a8ddbcc9eee570848c33bd4ab
sayhanne <sayhannee@gmail.com>
2022-04-17 16:02:36 +0300

    Collider and continous movement fixed. Character fixed

---
+++ VRelaxApp/Assets/Normal/Examples/VR Player/Scripts/Scripts/Hand.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public float speed;
+    Animator animator;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //Debug.Log("Selam "+animator.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    internal void SetGrip(float v)
+    {
+        gripTarget = v;
+    }
+
+    internal void SetTrigger(float v)
+    {
+        triggerTarget = v;
+    }
+
+    void AnimateHand()
+    {
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            animator.SetFloat("Grip", gripCurrent);
+        }
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat("Trigger", triggerCurrent);
+        }
+    }
+}
